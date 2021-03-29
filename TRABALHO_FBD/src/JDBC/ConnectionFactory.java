122d1020e803796b4dc5e0767dedd28ce100592f
Carlos <carlosferreira@alu.ufc.br>
2021-03-29 10:08:58 -0300

    Uploading source code

---
+++ TRABALHO_FBD/src/JDBC/ConnectionFactory.java
@@ -0,0 +1,23 @@
+package JDBC;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactory {
+	private final String ip = "localhost"; 
+	private final Integer port = 5432;
+	private final String user = "postgres";
+	private final String password = "postgres";
+	private final String database = "Trabalho";
+    
+	public Connection getConnection() /*throws ClassNotFoundException, SQLException*/ {
+        try {
+           // Class.forName("org.postgresql.Driver");
+            return DriverManager.getConnection("jdbc:postgresql://"+ip+":"+port+"/"+database, user, password); 
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+	
+}
